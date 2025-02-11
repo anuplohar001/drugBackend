f67b0b6b7ede4c1e961f58b9e7770b4f08ce809e
anuplohar001 <anuplohar001@gmail.com>
2025-02-11 11:00:46 +0530

    liveLink

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://drug-recommendation.vercel.app"],
     methods: "GET, POST, PUT, DELETE, PATCH, HEAD"
 }))
 
